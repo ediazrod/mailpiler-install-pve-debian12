7e8ce1331e472ddc9defd2d65e34fb1ca4973a9e
Julian Altmann <63753470+thisIsMasterGerhard@users.noreply.github.com>
2022-09-02 01:38:29 +0200

    Update install-mailpiler_0.5.sh

--- install-mailpiler_0.5.sh
+++ install-mailpiler_0.5.sh
@@ -29,9 +29,9 @@ echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt
 
 apt update && apt full-upgrade -y
 
-apt install -y mc sysstat build-essential libwrap0-dev libpst-dev tnef libytnef0-dev unrtf catdoc libtre-dev tre-agrep poppler-utils libzip-dev unixodbc libpq5 software-properties-common libpoppler-dev openssl libssl-dev memcached telnet nginx mariadb-server default-libmysqlclient-dev python-mysqldb gcc libwrap0 libzip4 latex2rtf latex2html catdoc tnef libpq5 zipcmp zipmerge ziptool libsodium23
+apt install -y mc sysstat build-essential libwrap0-dev libpst-dev tnef libytnef0-dev unrtf catdoc libtre-dev tre-agrep poppler-utils libzip-dev unixodbc libpq5 software-properties-common libpoppler-dev openssl libssl-dev memcached telnet nginx mariadb-server default-libmysqlclient-dev gcc libwrap0 libzip4 latex2rtf latex2html catdoc tnef libpq5 zipcmp zipmerge ziptool libsodium23
 
-apt update && apt install -y php$PHP_VERSION-{fpm,common,ldap,mysql,cli,opcache,phpdbg,gd,memcache,json,readline,zip}
+apt update && apt install -y php7.4-fpm php7.4-common php7.4-ldap php7.4-mysql php7.4-cli php7.4-opcache php7.4-phpdbg php7.4-gd php7.4-memcache php7.4-json php7.4-readline php7.4-zip
 
 apt purge -y postfix
 
@@ -118,22 +118,17 @@ server {\n\
 cp /usr/local/etc/piler/config-site.php /usr/local/etc/piler/config-site.php.bak
 sed -i "s|\$config\['SITE_URL'\] = .*|\$config\['SITE_URL'\] = 'https://$PILER_DOMAIN/';|" /usr/local/etc/piler/config-site.php
 cat >> /usr/local/etc/piler/config-site.php <<EOF
-
 // CUSTOM
 \$config['PROVIDED_BY'] = '$PILER_DOMAIN';
 \$config['SUPPORT_LINK'] = 'https://$PILER_DOMAIN';
 \$config['COMPATIBILITY'] = '';
-
 // fancy features.
 \$config['ENABLE_INSTANT_SEARCH'] = 1;
 \$config['ENABLE_TABLE_RESIZE'] = 1;
-
 \$config['ENABLE_DELETE'] = 1;
 \$config['ENABLE_ON_THE_FLY_VERIFICATION'] = 1;
-
 // general settings.
 \$config['TIMEZONE'] = 'Europe/Berlin';
-
 // authentication
 // Enable authentication against an imap server
 //\$config['ENABLE_IMAP_AUTH'] = 1;
@@ -143,7 +138,6 @@ cat >> /usr/local/etc/piler/config-site.php <<EOF
 //\$config['IMAP_HOST'] = '$SMARTHOST';
 //\$config['IMAP_PORT'] =  993;
 //\$config['IMAP_SSL'] = true;
-
 // authentication against an ldap directory (disabled by default)
 //\$config['ENABLE_LDAP_AUTH'] = 1;
 //\$config['LDAP_HOST'] = '$SMARTHOST';
@@ -154,7 +148,6 @@ cat >> /usr/local/etc/piler/config-site.php <<EOF
 //\$config['LDAP_AUDITOR_MEMBER_DN'] = '';
 //\$config['LDAP_ADMIN_MEMBER_DN'] = '';
 //\$config['LDAP_BASE_DN'] = 'ou=Benutzer,dc=krs,dc=local';
-
 // authentication against an Uninvention based ldap directory 
 //\$config['ENABLE_LDAP_AUTH'] = 1;
 //\$config['LDAP_HOST'] = '$SMARTHOST';
@@ -168,7 +161,6 @@ cat >> /usr/local/etc/piler/config-site.php <<EOF
 //\$config['LDAP_ACCOUNT_OBJECTCLASS'] = 'person';
 //\$config['LDAP_DISTRIBUTIONLIST_OBJECTCLASS'] = 'person';
 //\$config['LDAP_DISTRIBUTIONLIST_ATTR'] = 'mailAlternativeAddress';
-
 // special settings.
 \$config['MEMCACHED_ENABLED'] = 1;
 \$config['SPHINX_STRICT_SCHEMA'] = 1; // required for Sphinx $SPHINX_VERSION, see https://bitbucket.org/jsuto/piler/issues/1085/sphinx-331.
@@ -178,5 +170,3 @@ nginx -t && systemctl restart nginx
 
 apt autoremove -y
 apt clean -y
-
-
